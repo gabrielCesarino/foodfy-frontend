8955376291251ef8e9536cd4c6140d4a67a8b339
gabrielCesarino <gahnrq@hotmail.com>
2020-07-07 21:08:42 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require ('express')
+const server = express ()
+const nunjucks = require ('nunjucks')
+
+
+server.set("view engine", "njk")
+
+server.use(express.static('public'))
+
+nunjucks.configure("views", {
+    express:server,
+    autoescape: false
+})
+
+server.get("/", function(req, res){
+    res.render("index")
+})
+
+
+
+
+
+server.listen(5000, function(req, res){
+})
